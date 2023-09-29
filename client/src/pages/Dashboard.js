71024a6745f2a8839b9a9ddd0071af5f5f45dcb1
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2023-09-30 03:05:56 +0530

    updated admin

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -10,6 +10,8 @@ import { useState } from "react";
 import { Divider } from "@nextui-org/react";
 import AddFilled from "../images/AddFilled.svg";
 import releaseForm from "../images/releaseForm.svg";
+import expandArrowUp from "../images/expandArrowUp.svg";
+import linkLogo from "../images/linkLogo.svg";
 
 function DashButton({ children }) {
   const [active, setActive] = useState(false);
@@ -41,6 +43,49 @@ function DashButton({ children }) {
   );
 }
 
+function FormControlsSection(){
+  const [active, setActive] = useState(false);
+  let put;
+  if (active) {
+    put = (
+      <div className="flex flex-col gap-5">
+      <Button className="bg-black h-20 border-2 border-blue-600 flex flex-row items-center content-center" onClick={()=>setActive(false)}>
+            <p className="text-3xl font-bold ml-6">Form options</p>
+            <img className="h-9 w-9" src={expandArrowUp} alt="menu" />
+          </Button>
+          <Card className="border-2 border-blue-600 bg-black">
+            <CardHeader className="flex flex-row justify-center items-start my-2">
+            <div className="flex flex-col items-center">
+              <p className="font-['Roboto'] mb-2 text-xl">Filled by</p>
+              <p className="text-5xl font-semibold">30</p>
+            </div>
+            <Divider orientation="vertical" className="bg-white h-20 mx-4 mt-1" />
+            <div className="flex flex-col items-center m-0 p-0 gap-1">
+              <p className="font-['Roboto'] mb-2 text-xl">Open Since</p>
+              <p className="text-4xl font-semibold ">28-9-23</p>
+            </div>
+            </CardHeader>
+          </Card>
+          <Card className="bg-blue-600 px-7">
+            <CardBody className="flex flex-row items-center">
+            <img className="h-14 w-14 mr-1" src={linkLogo} alt="menu" />
+            <div className="flex flex-col p-3 gap-2"><p className="text-2xl font-bold">Form link</p><p className="text-xl grow break-words">ergteterwrtve564eybe6yb46b746</p></div>
+            </CardBody>
+          </Card>
+          </div>
+    );
+  } else {
+    put = (
+      <Button className="bg-blue-600 h-20" onClick={()=>setActive(true)}>
+            <img className="h-9 w-9" src={releaseForm} alt="menu" />
+            <p className="text-3xl font-bold">Release form</p>
+          </Button>
+    );
+  }
+  return (
+    <>{put}</>
+  );
+}
 export default function Dashboard() {
   return (
     <>
@@ -83,10 +128,7 @@ export default function Dashboard() {
             </div>
           </CardBody>
         </Card>
-        <Button className="bg-blue-600 h-20">
-            <img className="h-9 w-9" src={releaseForm} alt="menu" />
-            <p className="text-3xl font-bold">Release form</p>
-          </Button>
+        <FormControlsSection />
       </div>
     </>
   );
