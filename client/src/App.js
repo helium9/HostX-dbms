3f6dec5ea5b909c86f9f2b64393a34995c4dc15c
xenom2004 <117723390+xenom2004@users.noreply.github.com>
2023-09-29 19:58:07 +0530

    done merge

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import LandingPage from "./pages/LandingPage"
 import Dashboard from "./pages/Dashboard"
 import Preferences from "./pages/Preferences"
+import Dashboard2 from "./pages/test";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<LandingPage />}></Route>
       <Route path="/admin" element={<Dashboard/>}></Route>
       <Route path="/form" element={<Preferences />}></Route>
+      <Route path="/test" element={<Dashboard2 />}></Route>
     </Routes>
     </>
   );
