3c15b781d5b7ffeef010256083b00fe1e4b34a28
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2023-09-29 16:40:38 +0530

    next ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import {Routes, Route} from "react-router-dom";
 import LandingPage from "./pages/LandingPage"
 import Dashboard from "./pages/Dashboard"
 import Preferences from "./pages/Preferences"
+import {NextUIProvider} from "@nextui-org/react";
 
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<LandingPage />}></Route>
-      <Route path="/admin" element={<Dashboard />}></Route>
+      <Route path="/admin" element={<NextUIProvider><Dashboard /></NextUIProvider>}></Route>
       <Route path="/form" element={<Preferences />}></Route>
     </Routes>
     </>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
+  darkMode: "class",
+  plugins: [nextui()],
   plugins: [],
 }
 
