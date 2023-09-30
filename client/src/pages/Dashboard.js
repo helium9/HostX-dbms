c6635d448960b28505a40c54a35cc5271a021567
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2023-09-30 14:27:12 +0530

    admin

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -102,9 +102,9 @@ function FormControlsSection() {
     );
   } else {
     put = (
-      <Button className="bg-blue-600 h-20" onClick={() => setActive(true)}>
-        <img className="h-9 w-9" src={releaseForm} alt="menu" />
-        <p className="text-3xl font-bold">Release form</p>
+      <Button className="bg-blue-600 h-20 min-w-fit" onClick={() => setActive(true)}>
+        <img className="h-8 w-8" src={releaseForm} alt="menu" />
+        <p className="text-3xl font-bold lg:text-2xl">Release form</p>
       </Button>
     );
   }
@@ -114,8 +114,8 @@ export default function Dashboard() {
   return (
     <>
       <NavbarComponent />
-      <div className="flex flex-col gap-5 p-7 font-['Poppins'] lg:flex-row lg:flex-wrap">
-        <Card className="rounded-[12px] border-slate-800 border-2 lg:w-fit lg:py-8 lg:m-0">
+      <div className="flex flex-col gap-5 p-7 font-['Poppins'] lg:flex-row lg:flex-no-wrap lg:px-32">
+        <Card className="rounded-[12px] border-slate-800 border-2 lg:py-8 lg:m-0 shrink-0">
           <CardHeader className="gap-4 px-6 h-32 flex lg:flex-col lg:h-72 lg:items-center lg:w-64">
             <img
               className="h-full p-2 opacity-90 lg:p-0 lg:m-0"
@@ -133,7 +133,7 @@ export default function Dashboard() {
             />
           </CardHeader>
         </Card>
-        <Card className="rounded-[12px] border-slate-800 border-2 px-9 lg:w-fit lg:grow">
+        <Card className="rounded-[12px] border-slate-800 border-2 px-9  lg:grow">
           <CardHeader className="m-0 p-0">
             <p className="font-bold text-2xl mt-6 mb-4">Registered Hostels</p>
           </CardHeader>
@@ -157,7 +157,7 @@ export default function Dashboard() {
             </div>
           </CardBody>
         </Card>
-        <FormControlsSection />
+        <FormControlsSection className="shrink-0" />
       </div>
     </>
   );
