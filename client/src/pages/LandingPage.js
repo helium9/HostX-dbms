d7f013cb7b5e4af7bca975c582c043fdce434074
xenom2004 <117723390+xenom2004@users.noreply.github.com>
2023-10-02 00:37:40 +0530

    landing page image bk

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import FooterComponent from "../components/FooterComponent";
+
 import NavbarComponent from "../components/NavbarComponent";
 import pyramid from "../images/2_3pyramid.svg"
 import donut from "../images/5_3donut.svg"
 import strip from "../images/18_3strip.svg"
 import wave from "../images/Line 1wave.svg"
 import slice from "../images/Mask groupwatermelon.svg"
+import person from "../images/Frame 36persons.svg"
 import bgcombine from "../images/Frame 31cb.svg"
 import {Card, CardBody,CardHeader,CardFooter,Link, Image, Button, Progress} from "@nextui-org/react";
 
@@ -13,8 +15,18 @@ export default function LandingPage(){
     return(<>
         <div className="relative flex flex-col min-h-screen">
           <NavbarComponent />
-          <img className="w-full" src={bgcombine}></img>
-          <div className="absolute top-20 left-0 w-full h-full bg-opacity-10 bg-black">
+          <img className="absolute left-0 top-0 h-[200px] sm:w-3/4 sm:h-[250px] " src={wave}></img>
+          <img className="absolute hidden lg:block left-[65%] top-[60px] w-[100px] h-[100px]" src={pyramid}></img>
+          
+          <img src={person} className="w-[120px]  h-[128px] left-3/4 top-[156px] sm:w-1/8 md:top-[60px] md:h-[185px] md:h-[280px] md:w-[20%] absolute"></img>
+          <img src={slice} className="absolute w-1/8 h-[100px] top-[230px] sm:top-[280px] md:left-[7%] lg:left-[12%] xl:left-[14%]  left-1/8 "></img>
+          <img src={strip} className="absolute w-[30%] h-[180px] hidden md:block top-[235px]  left-1/2"></img>
+          <img src={donut} className="absolute w-[15%] h-[100px] left-[85%] top-[400px] md:left-[90%] xl:top-[100px] "></img>
+          
+          
+          
+          
+          <div className="absolute top-20 left-0 w-full h-full bg-opacity-0 bg-black">
           
           <div className="  grid mx-8 mt-12 md:mx-24 lg:mx-48 xl:mx-64 2xl:mx-80">
           <div className=" font-[poppins] text-5xl lg:text-6xl  justify-self-start">Seamlessly<span className="text-blue-500"> Allot</span> Rooms,</div>
