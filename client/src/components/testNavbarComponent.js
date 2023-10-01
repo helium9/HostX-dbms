e9aeb6aecd805636e8866f9dfe7f78c0f5998f12
Parth Deshmukh <deshmukhparth293@gmail.com>
2023-10-02 01:08:33 +0530

    activatingToggleMenu

---
+++ client/src/components/testNavbarComponent.js
@@ -0,0 +1,89 @@
+import React from "react";
+import menuLogo from "../images/menu.svg";
+import AboutUs from "../pages/AboutUs.js";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  menuItems,
+} from "@nextui-org/react";
+
+export default function NavbarComponent() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const menuItems = [
+    "Home",
+    "Docs",
+    "About Us",
+    "Log In",
+    "Sign Up",
+  ];
+
+  return (
+    <Navbar
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}>
+      <NavbarBrand>
+        <p className="font-bold text-inherit italic text-3xl">HostX</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-x-16" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#" className="text-xl">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page" className="text-xl">
+            Docs
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/AboutUs" className="text-xl">
+            About us
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#" className="text-xl">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button
+            as={Link}
+            color="primary"
+            href="#"
+            variant="flat"
+            className="invisible sm:visible text-xl h-10 rounded-lg"
+          >
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+      </NavbarContent>
+      <NavbarMenu className="navbar-menu absolute left-full">
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
