45ed9e4eb980c70191a32f7185b0a162f7ba557f
xenom2004 <117723390+xenom2004@users.noreply.github.com>
2023-10-01 18:18:56 +0530

    l

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -34,35 +34,37 @@ export default function Dashboard2() {
     </div>
     </div>
     </div>
-    <Input
-        label="Search"
-        isClearable
-        radius="lg"
-        classNames={{
-          label: "text-5xl dark:text-white/90",
-          input: [
-            "bg-transparent",
-            "text-6xl dark:text-white/90",
-            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-          ],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "shadow-xl",
-            "bg-default-200/50",
-            "dark:bg-default/60",
-            "backdrop-blur-xl",
-            "backdrop-saturate-200",
-            "hover:bg-default-200/70",
-            "dark:hover:bg-default/70",
-            "group-data-[focused=true]:bg-default-200/50",
-            "dark:group-data-[focused=true]:bg-default/60",
-            "!cursor-text",
-          ],
-        }}
-        placeholder="Type to search..."
-      />
+    
     <FooterComponent className="mt-auto" />
     
     </>
   );
 }
+
+// <Input
+//         label="Search"
+//         isClearable
+//         radius="lg"
+//         classNames={{
+//           label: "text-5xl dark:text-white/90",
+//           input: [
+//             "bg-transparent",
+//             "text-6xl dark:text-white/90",
+//             "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+//           ],
+//           innerWrapper: "bg-transparent",
+//           inputWrapper: [
+//             "shadow-xl",
+//             "bg-default-200/50",
+//             "dark:bg-default/60",
+//             "backdrop-blur-xl",
+//             "backdrop-saturate-200",
+//             "hover:bg-default-200/70",
+//             "dark:hover:bg-default/70",
+//             "group-data-[focused=true]:bg-default-200/50",
+//             "dark:group-data-[focused=true]:bg-default/60",
+//             "!cursor-text",
+//           ],
+//         }}
+//         placeholder="Type to search..."
+//       />
