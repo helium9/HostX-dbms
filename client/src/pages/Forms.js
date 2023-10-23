25b86367c13eb581fc2b361c99771ac6c81dd065
Parth Deshmukh <deshmukhparth293@gmail.com>
2023-10-23 08:59:51 +0530

    formcorrection

--- client/src/pages/Forms.js
+++ client/src/pages/Forms.js
@@ -1,5 +1,5 @@
 import NavbarComponent from "../components/NavbarComponent";
-import { useState } from "react";
+import { useState,useRef } from "react";
 import { Button } from "@nextui-org/react";
 import whiteTick from "../images/whiteTick.svg";
 import FooterComponent from "../components/FooterComponent";
@@ -7,6 +7,7 @@ import React from 'react';
 import {Input} from "@nextui-org/react";
 import {useForm,Controller} from 'react-hook-form';
 import {DevTool} from '@hookform/devtools';
+import axios from 'axios'
 
 
 // function InputComponent({ id, children = "Roll number", isDisabled = false }) {
@@ -37,12 +38,13 @@ import {DevTool} from '@hookform/devtools';
 //   );
 // }
 
-function InputComponent(){
+function InputComponent(props){
   const [value, setValue] = React.useState("");
 
   return (
     <div className="w-full flex flex-col gap-2 max-w-[240px] group">
       <Input
+      
         label="Roommate Preference"
         classNames={{
           label: [
@@ -65,10 +67,17 @@ function InputComponent(){
             "group",
           ]
         }}
+        isDisabled={props.isDisabled}
         placeholder="220001001"
         type="number"
-        value={value}
+        value={props.value}
         onValueChange={setValue}
+        name={props.name}
+        onChange={props.onChange}
+        // onChange={handleInput}
+        // placeholder={props.placeholder}
+        // onChange={e=>props.setUsername(e.target.value)}
+        // ref={props.refer}
       />
       {/* <p className="text-default-500 text-small">Input value: {value}</p> */}
     </div>
@@ -77,53 +86,82 @@ function InputComponent(){
 }
 
 export default function Forms() {
+ 
+  // const [username, setUsername] = useState("");
+  // const usernameRef=useRef()
 
-  const {control,handleSubmit}=useForm({
-    defaultValues:{
+  // console.log(username)
+  
+  // const handleSubmit=(e)=>{
+  //   e.preventDefault();
+  //   console.log(usernameRef);
+  // }
 
+  const [post,setPost]=useState({
+    P1:'',
+    P2:'',
+    P3:'',
+    P4:'',
+    P5:'',
+    P6:''
+  })
+
+  // const handleSubmit=(e)=>{
+  //   e.preventDefault();
+  //   const data=new FormData(e.target)
+  //   console.log(Object.fromEntries(data.entries()));
+    
+  // }
+  
+  const handleInput=(event)=>{
+    setPost({...post,[event.target.name]: event.target.value})
+
+  }
+
+  // const handleSubmit=(event)=>{
+  //   event.preventDefault();
+  //   axios.post('https://jsonplaceholder.typicode.com/posts',post)
+  //   .then(response=> console.log(response))
+  //   .catch(err=> console.log(err))
+  //   // const data=new FormData(event.target)
+  //   // console.log(Object.fromEntries(data.entries()));
+
+  // }
+  const handleSubmit = async (event) => { 
+    event.preventDefault();
+    
+    try {
+      const response = await axios.post('https://jsonplaceholder.typicode.com/posts', post); 
+      console.log(response);
+    } catch (err) {
+      console.error(err);
     }
-  });
-
-  const onSubmit =(values) =>{
-    console.log({values});
-  } 
+  }
+  
+  
 
   return (
     <>
       <div className="flex flex-col min-h-screen">
         <NavbarComponent />
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-96 lg:w-[32rem] self-center gap-3 mb-20 lg:gap-4">
-  
-          <p className="font-bold lg:text-4xl text-3xl m-4 lg:m-6 lg:ml-0 ml-0">
+        <form onSubmit={handleSubmit} className="flex flex-col w-96 lg:w-[32rem] self-center gap-3 mb-20 lg:gap-4">
+           <p className="font-bold lg:text-4xl text-3xl m-4 lg:m-6 lg:ml-0 ml-0">
             Your preferences
-          </p>
-          {/* <Controller
-  name="P1"
-  control={control}
-  render={({ field }) => (
-    <InputComponent id="P1" {...field} />
-  )}
-/> */}
-          <Controller
-          render={({props})=><InputComponent {...props} id="P1"/>}
-          // as={InputComponent}
-          name="P1"
-          control={control}
-          />
-
-
-          {/* <InputComponent id="P1" isDisabled={true}>220001001</InputComponent> */}
-          {/* <InputComponent id="P2" />
-          <InputComponent id="P3" />
-          <InputComponent id="P4" />
-          <InputComponent id="P5" />
-          <InputComponent id="P6" /> */}
+           </p>
+          {/* <InputComponent id="P1" isDisabled={true}>220001001 placeholder="user" </InputComponent> */}
+          {/* <InputComponent id="P1"  refer={usernameRef} /> */}
+          <InputComponent /*isDisabled*/ value={post.P1} name="P1" id="P1" onChange={handleInput}  />
+          <InputComponent name="P2" id="P2" value={post.P2} onChange={handleInput} />
+          <InputComponent name="P3" id="P3" value={post.P3} onChange={handleInput} />
+          <InputComponent name="P4" id="P4" value={post.P4} onChange={handleInput} />
+          <InputComponent name="P5" id="P5" value={post.P5} onChange={handleInput} />
+          <InputComponent name="P6" id="P6" value={post.P6} onChange={handleInput}/>
           <Button type="submit" className="rounded-lg h-12 w-32 bg-blue-600 flex flex-row items-center m-0 p-0 gap-1 self-end mt-6">
             <img className="h-7 w-7" src={whiteTick} alt="menu" />
             <p className="text-xl font-bold">Submit</p>
           </Button>
         </form>
-        <DevTool control={control}/>   
+        {/* <DevTool control={control}/>    */}
       </div>
       <FooterComponent />
     </>
