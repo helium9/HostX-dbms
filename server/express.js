a647e29ea37fff8ad22b46950ef6a99ab302f350
Parth Deshmukh <deshmukhparth293@gmail.com>
2023-10-24 15:44:03 +0530

    expressChanges

--- server/express.js
+++ server/express.js
@@ -12,10 +12,6 @@ app.use(cors());
 
 const customDirectory = path.join(__dirname, '../client/src/pages/'); 
 
-// app.get('/form',(req, res) => { 
-//     res.sendFile('Forms.js', { root: customDirectory });
-
-// });
 
 app.post('/api/submit', (req, res) => {
   const formData = req.body;
