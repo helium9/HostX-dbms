7cb322171d4fe560455781483e139746e811897a
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2023-10-30 17:20:31 +0530

    Fixed buttons in dashboard

--- client/src/pages/DashboardTest.js
+++ client/src/pages/DashboardTest.js
@@ -14,56 +14,24 @@ import linkLogo from "../images/linkLogo.svg";
 import stopForm from "../images/stopForm.svg";
 import viewForm from "../images/viewForm.svg";
 import whiteEditPencil from "../images/whiteEditPencil.svg";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
-import axios from 'axios'
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Checkbox,
+  Input,
+  Link,
+} from "@nextui-org/react";
+import axios from "axios";
 
-// import {MailIcon} from './MailIcon.jsx';
-// import {LockIcon} from './LockIcon.jsx';
 
-import React from "react";
 
-// export const MailIcon = (props) => (
-//   <svg
-//     aria-hidden="true"
-//     fill="none"
-//     focusable="false"
-//     height="1em"
-//     role="presentation"
-//     viewBox="0 0 24 24"
-//     width="1em"
-//     {...props}
-//   >
-//     <path
-//       d="M17 3.5H7C4 3.5 2 5 2 8.5V15.5C2 19 4 20.5 7 20.5H17C20 20.5 22 19 22 15.5V8.5C22 5 20 3.5 17 3.5ZM17.47 9.59L14.34 12.09C13.68 12.62 12.84 12.88 12 12.88C11.16 12.88 10.31 12.62 9.66 12.09L6.53 9.59C6.21 9.33 6.16 8.85 6.41 8.53C6.67 8.21 7.14 8.15 7.46 8.41L10.59 10.91C11.35 11.52 12.64 11.52 13.4 10.91L16.53 8.41C16.85 8.15 17.33 8.2 17.58 8.53C17.84 8.85 17.79 9.33 17.47 9.59Z"
-//       fill="currentColor"
-//     />
-//   </svg>
-// );
-
-// export const LockIcon = (props) => (
-//     <svg
-//       aria-hidden="true"
-//       fill="none"
-//       focusable="false"
-//       height="1em"
-//       role="presentation"
-//       viewBox="0 0 24 24"
-//       width="1em"
-//       {...props}
-//     >
-//       <path
-//         d="M12.0011 17.3498C12.9013 17.3498 13.6311 16.6201 13.6311 15.7198C13.6311 14.8196 12.9013 14.0898 12.0011 14.0898C11.1009 14.0898 10.3711 14.8196 10.3711 15.7198C10.3711 16.6201 11.1009 17.3498 12.0011 17.3498Z"
-//         fill="currentColor"
-//       />
-//       <path
-//         d="M18.28 9.53V8.28C18.28 5.58 17.63 2 12 2C6.37 2 5.72 5.58 5.72 8.28V9.53C2.92 9.88 2 11.3 2 14.79V16.65C2 20.75 3.25 22 7.35 22H16.65C20.75 22 22 20.75 22 16.65V14.79C22 11.3 21.08 9.88 18.28 9.53ZM12 18.74C10.33 18.74 8.98 17.38 8.98 15.72C8.98 14.05 10.34 12.7 12 12.7C13.66 12.7 15.02 14.06 15.02 15.72C15.02 17.39 13.67 18.74 12 18.74ZM7.35 9.44C7.27 9.44 7.2 9.44 7.12 9.44V8.28C7.12 5.35 7.95 3.4 12 3.4C16.05 3.4 16.88 5.35 16.88 8.28V9.45C16.8 9.45 16.73 9.45 16.65 9.45H7.35V9.44Z"
-//         fill="currentColor"
-//       />
-//     </svg>
-//   );
 
 function DashButton({ children }) {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [active, setActive] = useState(false);
   let put;
   if (active) {
@@ -73,130 +41,86 @@ function DashButton({ children }) {
         <img className="h-5 w-5" src={blueCross} alt="menu" />
         {/* <img className="h-5 w-5" src={blueEditPencil} alt="menu" /> */}
         <>
-        <Button onPress={onOpen} size="sm" radius="lg" className="h-8 min-w-fit bg-black//[.06]"><img className="h-5 w-5" src={blueEditPencil} alt="menu" /></Button>
-      <Modal 
-        isOpen={isOpen} 
-        onOpenChange={onOpenChange}
-        placement="top-center"
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1 text-2xl">Edit</ModalHeader>
-              <ModalBody>
-              <Input
-                    autoFocus
-                    /*endContent={
-                      <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                    }*/
-                    label="Name"
-                    placeholder="Edit hostel name"
-                    variant="bordered"
-                    id="hostelNameEdit"
-                    name="hostelNameEdit"
-                    // onChange={handleInput}
-                    // value={post.hostelName}
-                    classNames={{
-                      label: "text-lg",
-                      input: [
-                        "placeholder:text-xl"
-                      ],
-                      innerWrapper: "bg-transparent",
-                      inputWrapper: [
-                     "h-20",
-                      ],
-                    }}
-                    // value={post}
-                  />
-                  <Input
-                   /* endContent={
+          <img
+            onClick={onOpen}
+            className="h-5 w-5"
+            src={blueEditPencil}
+            alt="menu"
+          />
+          <Modal
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            placement="top-center"
+          >
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1 text-2xl">
+                    Edit
+                  </ModalHeader>
+                  <ModalBody>
+                    <Input
+                      autoFocus
+                      label="Name"
+                      placeholder="Edit hostel name"
+                      variant="bordered"
+                      id="hostelNameEdit"
+                      name="hostelNameEdit"
+                      classNames={{
+                        label: "text-lg",
+                        input: ["placeholder:text-xl"],
+                        innerWrapper: "bg-transparent",
+                        inputWrapper: ["h-20"],
+                      }}
+                    />
+                    <Input
+                      label="5 unit rooms"
+                      placeholder="Edit number of 5 unit rooms"
+                      type="number"
+                      variant="bordered"
+                      id="hostelUnit5Edit"
+                      name="hostelUnit5Edit"
+                      classNames={{
+                        label: "text-lg",
+                        input: ["placeholder:text-xl", "text-xl"],
+                        innerWrapper: "bg-transparent",
+                        inputWrapper: ["h-20"],
+                      }}
+                    />
+                    <Input
+                      /* endContent={
                       <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
                     }*/
-                    label="5 unit rooms"
-                    placeholder="Edit number of 5 unit rooms"
-                    type="number"
-                    variant="bordered"
-                    id="hostelUnit5Edit"
-                    name="hostelUnit5Edit"
-                    // onChange={handleInput}
-                    // value={post.hostelUnit5}
-                    classNames={{
-                      label: "text-lg",
-                      input: [
-                        "placeholder:text-xl",
-                        "text-xl"
-                      ],
-                      innerWrapper: "bg-transparent",
-                      inputWrapper: [
-                     "h-20",
-                      ],
-                    }}
-                    // value={post}
-                  />
-                  <Input
-                   /* endContent={
-                      <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                    }*/
-                    label="6 unit rooms"
-                    placeholder="Edit number of 6 unit rooms"
-                    type="number"
-                    variant="bordered"
-                    id="hostelUnit6Edit"
-                    name="hostelUnit6Edit"
-                    // onChange={handleInput}
-                    // value={post.hostelUnit6}
-                    classNames={{
-                      label: "text-lg",
-                      input: [
-                        "placeholder:text-xl",
-                        "text-xl"
-                      ],
-                      innerWrapper: "bg-transparent",
-                      inputWrapper: [
-                     "h-20",
-                      ],
-                    }}
-                    // value={post}
-                  />
-                {/* <Input
-                  // endContent={
-                  //   <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  // }
-                  label="Password"
-                  placeholder="Enter your password"
-                  type="password"
-                  variant="bordered"
-                /> */}
-                {/* <div className="flex py-2 px-1 justify-between">
-                  <Checkbox
-                    classNames={{
-                      label: "text-small",
-                    }}
-                  >
-                    Remember me
-                  </Checkbox>
-                  <Link color="primary" href="#" size="sm">
-                    Forgot password?
-                  </Link>
-                </div> */}
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Edit
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
+                      label="6 unit rooms"
+                      placeholder="Edit number of 6 unit rooms"
+                      type="number"
+                      variant="bordered"
+                      id="hostelUnit6Edit"
+                      name="hostelUnit6Edit"
+                      // onChange={handleInput}
+                      // value={post.hostelUnit6}
+                      classNames={{
+                        label: "text-lg",
+                        input: ["placeholder:text-xl", "text-xl"],
+                        innerWrapper: "bg-transparent",
+                        inputWrapper: ["h-20"],
+                      }}
+                    />
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="danger" variant="flat" onPress={onClose}>
+                      Close
+                    </Button>
+                    <Button color="primary" onPress={onClose}>
+                      Edit
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
+        </>
       </div>
-      // </Button>
-        
-      // </div>
     );
   } else {
     put = (
@@ -243,49 +167,49 @@ function FormControlsButton({ active, setActive }) {
   }
   return <>{put}</>;
 }
-{/* <Button onPress={onOpen} size="sm" radius="lg" className="h-8 min-w-fit bg-black//[.06]"><img className="w-8 h-8" src={AddFilled} alt="menu" /></Button> */}
-function FormPlusModal(){
+function FormPlusModal() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [post, setPost] = useState({
+    hostelName: "",
+    hostelUnit5: "",
+    hostelUnit6: "",
+  });
 
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const handleInput = (event) => {
+    setPost({ ...post, [event.target.name]: event.target.value });
+  };
 
-    const [post,setPost]=useState({
-      hostelName:'',
-      hostelUnit5:'',
-      hostelUnit6:'',
-    
-    })
-
-    const handleInput=(event)=>{
-      setPost({...post,[event.target.name]: event.target.value})
-  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // const response = await axios.post('https://jsonplaceholder.typicode.com/posts', post);
+      const response = await axios.post(
+        "http://localhost:8000/api/admin/submit",
+        post
+      );
+      console.log(response);
+    } catch (err) {
+      console.error(err);
     }
-
-    const handleSubmit = async (event) => { 
-      event.preventDefault();
-      
-      try {
-        // const response = await axios.post('https://jsonplaceholder.typicode.com/posts', post); 
-        const response = await axios.post('http://localhost:8000/api/admin/submit', post); 
-        console.log(response);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    return (
-      <>
-        <Button onPress={onOpen} size="sm" radius="lg" className="h-8 min-w-fit bg-black//[.06]"><img className="w-8 h-8" src={AddFilled} alt="menu" /></Button>
-        <Modal  
-          isOpen={isOpen} 
-          onOpenChange={onOpenChange}
-          placement="top-center"
-          size="lg"
-          // className="h-96"
-        >
-          <ModalContent>
-            {(onClose) => (
-              <>
-                <ModalHeader className="flex flex-col gap-1 text-2xl">Register a new hostel</ModalHeader>
-                <form onSubmit={handleSubmit}>
+  };
+  return (
+    <>
+      <button onClick={onOpen}>
+        <img className="w-8 h-8" src={AddFilled} alt="menu" />
+      </button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="top-center"
+        size="lg"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-2xl">
+                Register a new hostel
+              </ModalHeader>
+              <form onSubmit={handleSubmit}>
                 <ModalBody>
                   <Input
                     autoFocus
@@ -301,19 +225,14 @@ function FormPlusModal(){
                     value={post.hostelName}
                     classNames={{
                       label: "text-lg",
-                      input: [
-                        "placeholder:text-xl",
-                        "text-xl"
-                      ],
+                      input: ["placeholder:text-xl", "text-xl"],
                       innerWrapper: "bg-transparent",
-                      inputWrapper: [
-                     "h-20",
-                      ],
+                      inputWrapper: ["h-20"],
                     }}
                     // value={post}
                   />
                   <Input
-                   /* endContent={
+                    /* endContent={
                       <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
                     }*/
                     label="5 unit rooms"
@@ -326,20 +245,14 @@ function FormPlusModal(){
                     value={post.hostelUnit5}
                     classNames={{
                       label: "text-lg",
-                      input: [
-                        "placeholder:text-xl",
-                        "text-xl"
-
-                      ],
+                      input: ["placeholder:text-xl", "text-xl"],
                       innerWrapper: "bg-transparent",
-                      inputWrapper: [
-                     "h-20",
-                      ],
+                      inputWrapper: ["h-20"],
                     }}
                     // value={post}
                   />
                   <Input
-                   /* endContent={
+                    /* endContent={
                       <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
                     }*/
                     label="6 unit rooms"
@@ -352,18 +265,13 @@ function FormPlusModal(){
                     value={post.hostelUnit6}
                     classNames={{
                       label: "text-lg",
-                      input: [
-                        "placeholder:text-xl",
-                        "text-xl"
-                      ],
+                      input: ["placeholder:text-xl", "text-xl"],
                       innerWrapper: "bg-transparent",
-                      inputWrapper: [
-                     "h-20",
-                      ],
+                      inputWrapper: ["h-20"],
                     }}
                     // value={post}
                   />
-                    {/* <div className="flex py-2 px-1 justify-between">
+                  {/* <div className="flex py-2 px-1 justify-between">
                     <Checkbox
                       classNames={{
                         label: "text-small",
@@ -383,17 +291,17 @@ function FormPlusModal(){
                   <Button color="danger" variant="light" onPress={onClose}>
                     Close
                   </Button>
-                  <Button color="primary" onPress={onClose} type="submit" >
+                  <Button color="primary" onPress={onClose} type="submit">
                     Register
                   </Button>
                 </ModalFooter>
-                  </form>
-              </>
-            )}
-          </ModalContent>
-        </Modal>
-      </>
-    );
+              </form>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
 function FormControlsSection({ active }) {
@@ -658,8 +566,8 @@ export default function DashboardTest() {
 //     return (
 //       <>
 //         <Button onPress={onOpen} size="sm" radius="lg" className="h-8 min-w-fit bg-black//[.06]"><img className="w-8 h-8" src={AddFilled} alt="menu" /></Button>
-//         <Modal 
-//           isOpen={isOpen} 
+//         <Modal
+//           isOpen={isOpen}
 //           onOpenChange={onOpenChange}
 //           placement="top-center"
 //         >
