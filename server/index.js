5b576298e7b3b9ea0a78b4c76730fd31a4cdd9f3
Aditya Kshitiz <adityakshitiz2017@gmail.com>
2023-10-31 13:29:47 +0530

    updated package.json

--- server/index.js
+++ server/index.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const path = require('path'); 
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const path = require("path");
 const { fileURLToPath } = require("url");
-const cors=require('cors');
-const { v4: uuidv4 } = require('uuid');
-const passport = require('passport');
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+const passport = require("passport");
 const googleAuth = require("./routes/authentication");
 
 // const { router, sharedToken } = require("./routes/authentication");
 
-const jwt = require('jsonwebtoken');
-function con(){const connection = mysql.createConnection({
-  host: process.db_env.DB_HOST,         
-  user: process.db_env.DB_USER,    
-  password: process.db_env.DB_PASSWORD, 
-  database: process.db_env.DB_NAME, 
-});
-return connection;}
+const jwt = require("jsonwebtoken");
+function con() {
+  const connection = mysql.createConnection({
+    host: process.db_env.DB_HOST,
+    user: process.db_env.DB_USER,
+    password: process.db_env.DB_PASSWORD,
+    database: process.db_env.DB_NAME,
+  });
+  return connection;
+}
+
 const app = express();
-const port = 8000; 
+const port = 8000;
 
 require("./controllers/controller.tokenJWT");
 require("dotenv/config");
@@ -27,49 +30,50 @@ require("dotenv/config");
 app.use(express.json());
 app.use(cors());
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/auth", googleAuth);
 
+const customDirectory = path.join(__dirname, "../client/src/pages/");
 
-const customDirectory = path.join(__dirname, '../client/src/pages/'); 
-
-
-app.post('/api/submit', (req, res) => {
+app.post("/api/submit", (req, res) => {
   const formData = req.body;
   console.log(formData);
-  res.json({ message: 'Form data received and logged' });
+  res.json({ message: "Form data received and logged" });
 });
 
-app.post('/api/admin/submit', (req, res) => {
+app.post("/api/admin/submit", (req, res) => {
   const formAdminData = req.body;
   console.log(formAdminData);
-  res.json({ message: 'Form data received and logged' });
+  res.json({ message: "Form data received and logged" });
 });
 
-app.get("/",(req,res)=>{
-  res.send("Onto the backend")
-})
-let index=2;
+app.get("/", (req, res) => {
+  res.send("Onto the backend");
+});
+let index = 2;
 
-app.post("/api/admin/hostels",async (req,res)=>{
-  c=con();
+app.post("/api/admin/hostels", async (req, res) => {
+  c = con();
   const id = uuidv4();
-  const val=[id,req.body.hostelName,req.body.floors,"http://localhost:4000"];
-  index=index+1;
-  const insertQuery = 'INSERT INTO hostels VALUE (?,?, ?, ?);';
+  const val = [
+    id,
+    req.body.hostelName,
+    req.body.floors,
+    "http://localhost:4000",
+  ];
+  index = index + 1;
+  const insertQuery = "INSERT INTO hostels VALUE (?,?, ?, ?);";
 
   c.query(insertQuery, val, (err, results) => {
-      if (err) {
-        console.error('Error inserting data:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-      } else {
-        console.log('successfully created hostel ');
-        res.status(201).send();
-              
-      }
-    });
+    if (err) {
+      console.error("Error inserting data:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      console.log("successfully created hostel ");
+      res.status(201).send();
+    }
+  });
 
   // const password=req.body.Password;
   // const useri=req.body.user;
@@ -81,19 +85,17 @@ app.post("/api/admin/hostels",async (req,res)=>{
   // c=con();
   // const li=c.query(selectquery,b,(err,results)=>{if(err){throw err;}
   //         if(results[0]["count(*)"]>0){
-              
+
   //             res.status(202).send();
-              
-              
+
   //         }});
-  
- 
+
   // const hashedPassword = await bcrypt.hash(password, 10)
   // const user2 = { name: useri, password: hashedPassword }
   // users.push(user2);
   // const values = [useri,ph,hashedPassword];
   // console.log(hashedPassword);
-  
+
   // c.query(insertQuery, values, (err, results) => {
   //     if (err) {
   //       console.error('Error inserting data:', err);
@@ -101,23 +103,21 @@ app.post("/api/admin/hostels",async (req,res)=>{
   //     } else {
   //       console.log('successfully registered');
   //       res.status(201).send();
-              
+
   //     }
   //   });
-})  
-app.get("/api/admin/gethostelname",(req,res)=>{
-  c=con();
+});
+app.get("/api/admin/gethostelname", (req, res) => {
+  c = con();
   const getQuery = "Select HostelName from hostels ;";
-  const li=c.query(getQuery,(err,results)=>{if(err){throw err;}
-         
-              
-              res.send(results);
-              
-              
-          });
+  const li = c.query(getQuery, (err, results) => {
+    if (err) {
+      throw err;
+    }
 
-
-})
+    res.send(results);
+  });
+});
 // app.post('/api/logout', (req, res) => {
 //   // Perform the logout action here, such as invalidating the user's session or token.
 //   // For example, you can clear the session or remove the user's token from the server.
