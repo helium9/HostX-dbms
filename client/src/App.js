b9af448ac98fa6a0058ebde465cf48e201738876
Parth Deshmukh <deshmukhparth293@gmail.com>
2023-11-01 20:06:46 +0530

    ProtectionRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import Dashboard from "./pages/Dashboard"
 import Forms from "./pages/Forms";
 import AboutPage from "./pages/AboutUs";
@@ -8,20 +8,59 @@ import DashboardTest2 from "./pages/DashboardTest2"
 import AdminTable from "./pages/AdminTable"
 
 
+const User_Types={
+  PUBLIC:"Public User",
+  NORMAL_USER:"Normal User",
+  ADMIN_USER:"Admin User"
+}
+
+const CURRENT_USER_TYPE=User_Types.ADMIN_USER
+
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<LandingPage />}></Route>
-      <Route path="/admin" element={<Dashboard/>}></Route>
-      <Route path="/admin2" element={<DashboardTest/>}></Route>
-      <Route path="/admin3" element={<DashboardTest2/>}></Route>
-      <Route path="/form" element={<Forms />}></Route>
+
+      <Route path="/" element={
+        <PublicElement>
+            <LandingPage />
+        </PublicElement>}>
+      </Route>
+
+      <Route path="/admin" element={<AdminElement><Dashboard/></AdminElement>}></Route>
+      <Route path="/admin2" element={<AdminElement><DashboardTest/></AdminElement>}></Route>
+      <Route path="/admin3" element={<AdminElement><DashboardTest2/></AdminElement>}></Route>
+      <Route path="/form" element={<UserElement><Forms /></UserElement>}></Route>
       <Route path="/table" element={<AdminTable />}></Route>
-      <Route path="/about" element={<AboutPage />}></Route>
+      <Route path="/about" element={<UserElement><AboutPage /></UserElement>}></Route>
     </Routes>
     </>
   );
+}
+
+function PublicElement({children}){
+  return<>
+    {children}</>;
+}
+function UserElement({children}){
+  if(CURRENT_USER_TYPE===User_Types.ADMIN_USER || User_Types.NORMAL_USER){
+    return<>
+      {children}</>;}
+  else{
+    return <Navigate to={"/"}/>;
+
+    // <div>You do not have access to this page</div>
+  }
+}
+function AdminElement({children}){
+  if(CURRENT_USER_TYPE===User_Types.ADMIN_USER){
+    return<>
+      {children}</>;}
+  else{
+    return <Navigate to={"/"}/>;
+
+    // <div>You do not have access to this page</div>
+  }
 }
 
 export default App;
