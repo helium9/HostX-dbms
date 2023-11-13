4e836f0fbced1adbcce0452edd89d41f68403301
Parth Deshmukh <deshmukhparth293@gmail.com>
2023-11-13 20:39:19 +0530

    passportOauth

---
+++ server/passport.js
@@ -0,0 +1,6 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+
+const GOOGLE_CLIENT_ID =
+  "297512826619-mp37d56q19usd2qrl8ht6j564mnb34ng.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
